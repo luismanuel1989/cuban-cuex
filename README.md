9b8c7b78af013b1780807943c09e94812fbf2cff
Luis Manuel Vidal P <luismanuel.8902@gmail.com>
2023-01-09 12:14:02 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cuban-cuex
+
+A package to know about the cuban exchange in real time. CUP, MLC, USD.
---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param a sum a 
+ * @param b sum b 
+ * @returns result
+ */
+const add = (a,b) => a + b;
+
+export default {
+    add
+}
