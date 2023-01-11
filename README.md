ca84d58bad08a4f552d7959d84f3909c0d905057
Luis Manuel Vidal P <luismanuel.8902@gmail.com>
2023-01-11 15:45:56 -0400

    Updating functions and exports

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# Cuban-cuex
-
-A package to know about the cuban exchange in real time. CUP, MLC, USD.
+<h1 align="center">Cuban-cuex</h1>
+<h5 align="center">A package to know about the cuban exchange in real time. CUP, MLC, USD.</h5>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-/**
- * 
- * @param a sum a 
- * @param b sum b 
- * @returns result
- */
-export const add = (a,b) => a + b;
+import request from 'request-promise-native';
+import * as cheerio from 'cheerio';
 
+const result = await request('https://eltoque.com/')
+class CubanExchange{
+    constructor(result){
+        const $ = cheerio.load(result)
+        const TablePrices = $('.content').find('tbody tr').text()
+        this.EurPrice = TablePrices.split('EUR')[1].split(' CUP')[0]
+        this.UsdPrice = TablePrices.split('USD')[1].split(' CUP')[0]
+        this.MlcPrice = TablePrices.split('MLC')[1].split(' CUP')[0]
+        this.CadPrice = TablePrices.split('CAD')[1].split(' CUP')[0]
+        this.CalculationDate = new Date(parseInt($('.content time').map((i, x) => $(x).attr('datetime')).toArray()[0])).toLocaleString()
+    }
+}
+const cubanExchangeObj = new CubanExchange(result)
+export default {
+    ...cubanExchangeObj
+}
